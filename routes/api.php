3e658c4844875fd96f6f50efb3e47f94505ea225
Łukasz Jozwiak <admin@admin.pl>
2019-12-16 21:36:07 +0100

    Edit Api/AuthController

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'Api\AuthController@login')->middleware('auth:api');
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
